83ea6828f76e975b91ccebc26019f49a325458c9
Adrien Grand <adrien.grand@twenga.com>
2011-12-28 10:18:09 +0100

    Improved CharComparator.

--- src/java/net/jpountz/charsequence/CharComparator.java
+++ src/java/net/jpountz/charsequence/CharComparator.java
@@ -1,9 +1,12 @@
 package net.jpountz.charsequence;
 
+import java.util.Comparator;
+
+
 /**
  * Comparator of characters.
  */
-public interface CharComparator {
+public abstract class CharComparator {
 
 	/**
 	 * Default {@link CharComparator}. Returns:
@@ -18,6 +21,81 @@ public interface CharComparator {
 
 	};
 
-	int compare(char c1, char c2);
+	public abstract int compare(char c1, char c2);
+
+	public final int compare(CharSequence key1, int off1, int len1, CharSequence key2, int off2, int len2) {
+		int len = Math.min(len1, len2);
+		for (int i = 0; i < len; ++i) {
+			int cmp = compare(key1.charAt(off1 + i), key2.charAt(off2 + i));
+			if (cmp != 0) {
+				return cmp;
+			}
+		}
+		return len1 - len2;
+	}
+
+	public final int compare(CharSequence key1, CharSequence key2) {
+		return compare(key1, 0, key1.length(), key2, 0, key2.length());
+	}
+
+	public final int compare(char[] key1, int off1, int len1, char[] key2, int off2, int len2) {
+		int len = Math.min(len1, len2);
+		for (int i = 0; i < len; ++i) {
+			int cmp = compare(key1[off1 + i], key2[off2 + i]);
+			if (cmp != 0) {
+				return cmp;
+			}
+		}
+		return len1 - len2;
+	}
+
+	public final int compare(char[] key1, char[] key2) {
+		return compare(key1, 0, key1.length, key2, 0, key2.length);
+	}
+
+	public final int compare(CharSequence key1, int off1, int len1, char[] key2, int off2, int len2) {
+		int len = Math.min(len1, len2);
+		for (int i = 0; i < len; ++i) {
+			int cmp = compare(key1.charAt(off1 + i), key2[off2 + i]);
+			if (cmp != 0) {
+				return cmp;
+			}
+		}
+		return len1 - len2;
+	}
+
+	public final int compare(CharSequence key1, char[] key2) {
+		return compare(key1, 0, key1.length(), key2, 0, key2.length);
+	}
+
+	public final int compare(char[] key1, int off1, int len1, CharSequence key2, int off2, int len2) {
+		return - compare(key2, off2, len2, key1, off1, len1);
+	}
+
+	public final int compare(char[] key1, CharSequence key2) {
+		return compare(key1, 0, key1.length, key2, 0, key2.length());
+	}
+
+	public final <T extends CharSequence> Comparator<T> asCharSequenceComparator() {
+		return new Comparator<T>() {
+
+			@Override
+			public int compare(T arg0, T arg1) {
+				return CharComparator.this.compare(arg0, arg1);
+			}
+
+		};
+	}
+
+	public final Comparator<char[]> asCharArrayComparator() {
+		return new Comparator<char[]>() {
+
+			@Override
+			public int compare(char[] arg0, char[] arg1) {
+				return CharComparator.this.compare(arg0, arg1);
+			}
+
+		};
+	}
 
 }
