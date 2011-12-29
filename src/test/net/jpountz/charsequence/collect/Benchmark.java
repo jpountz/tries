f43d40f8faeced47df44da41cc27574d7d867e1d
Adrien Grand <adrien.grand@twenga.com>
2011-12-29 18:14:38 +0100

    Useless import.

--- src/test/net/jpountz/charsequence/collect/Benchmark.java
+++ src/test/net/jpountz/charsequence/collect/Benchmark.java
@@ -10,8 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import net.jpountz.charsequence.CharComparator;
-
 public class Benchmark {
 
 	public enum CharSequenceMapFactory {
